27d4cd61b60569022751886bd5ca640feed58342
Elias Ferede <feredeelias@gmail.com>
2020-09-10 15:59:03 -0700

    Updates

---
+++ precache-manifest.ce8014fe46e0214536b1adcec8e9fd2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f3c1c8e20a1972fedcd154b91a973b",
+    "url": "/React-Markdown-Generator/index.html"
+  },
+  {
+    "revision": "0cc49431c8e7036f29a9",
+    "url": "/React-Markdown-Generator/static/css/main.01f9fc31.chunk.css"
+  },
+  {
+    "revision": "0a0406be3ff6e9a666bf",
+    "url": "/React-Markdown-Generator/static/js/2.b8af8d8c.chunk.js"
+  },
+  {
+    "revision": "0cc49431c8e7036f29a9",
+    "url": "/React-Markdown-Generator/static/js/main.c8a18ae6.chunk.js"
+  },
+  {
+    "revision": "bce0a560c66b13964696",
+    "url": "/React-Markdown-Generator/static/js/runtime~main.cb02d8d1.js"
+  }
+]);
